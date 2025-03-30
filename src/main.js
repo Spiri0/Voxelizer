3ff6e3f6311493d34411bcd19f584c99f0dceceb
Attila Schroeder <attila-schroeder.79@gmail.com>
2025-03-30 23:45:43 +0200

    update

--- src/main.js
+++ src/main.js
@@ -17,7 +17,6 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { mergeGeometries } from 'three/addons/utils/BufferGeometryUtils.js';
 import Voxelizer from './voxelizer.js';
 import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
 
